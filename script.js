263b7bcbba18e79cef6fc6fc6eed302d98698f80
faithadewuyi <iyanuleke09@gmail.com>
2024-05-11 20:17:28 +0100

    Project Setup

---
+++ script.js
@@ -0,0 +1,4 @@
+let addEl = document.getElementById("add-expenses")
+let DateEl = document.getElementById ("expense-date");
+let amountEl = document.getElementById ("expense-amount");
+let descriptionEl = document.getElementById ("expense-description");
