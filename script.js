73d01776138b75a9903f845339b9e28d96e7a869
faithadewuyi <iyanuleke09@gmail.com>
2024-05-11 20:17:59 +0100

    Styling added

--- script.js
+++
@@ -1,4 +0,0 @@
-let addEl = document.getElementById("add-expenses")
-let DateEl = document.getElementById ("expense-date");
-let amountEl = document.getElementById ("expense-amount");
-let descriptionEl = document.getElementById ("expense-description");
